b9f6a2bd9aab46902edbddd1fbfd14062e573755
kalledus13 <kalledus13@gmail.com>
2024-11-04 19:19:31 -0500

    New files

---
+++ day_2.py
@@ -0,0 +1,43 @@
+print("hello"[1])
+print("hello"[0])
+print("hello"[-1])
+print(123_456_789)
+
+#Float = Floating Point Number
+#3.12412
+#4132.32
+
+
+#Boolean
+# we use for it True or False
+
+print(True)
+print(False)
+
+
+#type
+print(type("Hello")) # string type
+print(type(123 + 5)) #int type
+print(type(31.2 + 4.4)) #Float type
+print(type(True))            #boolean type
+
+print ("123" + "100")
+print(int("123") + int("100"))
+
+#name_of_the_user = input("Enter your name ")
+# = len(name_of_the_user)
+
+
+#print ("Number of letters in your name " + str(lenth_of_name))
+
+#-------------------------------------------------------------------------------
+
+print("My age: " + str(12))
+print(7 - 3)
+print(7 + 3)
+print(7 * 3)
+print(5 / 3)
+print(5 // 3)
+print(5**2)
+print(3 * 3 + 3 / 3 - 3) #from left to right but priority is умножение и потом деление
+print(3 * (3 + 3) / 3 - 3)
